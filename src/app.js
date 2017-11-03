c25bd073267d31fd4dd56b82bf1f619129720e83
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-11-03 16:00:08 +0100

    only leave dist files

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import JavaScriptCalculator from './components/JavaScriptCalculator';
-import './styles/styles.scss';
-
-ReactDOM.render(<JavaScriptCalculator />, document.getElementById('app'));
